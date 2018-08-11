ee831ae71606003062d5b9b2237af043b862e25f
Edward Hong <Edward.YSHF@gmail.com>
2018-08-11 00:05:11 -0400

    C++ learning updates

---
+++ Xcode_projects/hello_world/hello_world/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  hello_world
+//
+//  Created by  Edward Hong on 8/8/18.
+//  Copyright © 2018  Edward Hong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, world!\n";
+    return 0;
+}
+
+/*
+ Here's another way
+ to
+ create
+ multi-
+ line
+ comments
+ in
+ C++
+*/
